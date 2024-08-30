19f6256cc4409f1847e79cb9e14212697d05a8bc
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-08-30 18:05:57 +0600

    commited

--- scripts/utility.js
+++ scripts/utility.js
@@ -7,21 +7,50 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
-
 function getARandomAlphabet(){
-    //get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    console.log(alphabets);
 
-
-    //get a random index between 0-25
-
-    const randomNumber = Math.random()* 25;
+    const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
-    console.log(index);
-
     const alphabet = alphabets[index];
-    // console.log(index, alphabet);
+    console.log(index, alphabet);
     return alphabet;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getARandomAlphabet(){
+//     // get or create an aplhabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+
+//     //get a random index between 0-25
+
+//     const randomNumber = Math.random()*25;
+//     const index = Math.round(randomNumber);
+//     console.log(index);
+
+//     const alphabet = alphabets[index];
+//     console.log(index, alphabet);
+//     return alphabet;
+// }
