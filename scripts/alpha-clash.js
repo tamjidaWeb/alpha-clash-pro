8bebc93a80943ffac35124c886dc8cbb0b45371a
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-08-31 11:39:57 +0600

    commited

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -25,10 +25,43 @@ function handleKeyboardKeyUp(event){
     // check right or wrong key pressed
 
     if(playerPressed === expectedAlphabet){
-        console.log('you got a point!')
+        console.log('you got a point!');
+        console.log('you have pressed correctly',expectedAlphabet);
+        //update score:
+        //1. get the current score
+
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+
+
+        //2.increase the score by 1
+        const newScore = currentScore +1;
+
+        //3.show the update score
+        currentScoreElement.innerText = newScore;
+
+
+
+        //start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
     }
     else{
         console.log('dhur vai/ apu right key press korun');
+        //step 1: get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+
+        //step 2: reduce the life count
+        const newLife = currentLife - 1;
+
+
+        //step 3: display the updated life count 
+        currentLifeElement.innerText = newLife;
     }
    
 
@@ -39,7 +72,7 @@ function continueGame (){
 
     // step 1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('your alphabet', alphabet);
+    // console.log('your alphabet', alphabet);
 
     //set randomly generated alphabet to the screen (show it)
     const currentAlphabetElement = document.getElementById('current-alphabet');
