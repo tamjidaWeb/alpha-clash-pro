572ccea9571ef62b8934b2633222581d615fa8e7
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-08-31 12:57:57 +0600

    commited

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -29,18 +29,24 @@ function handleKeyboardKeyUp(event){
         console.log('you have pressed correctly',expectedAlphabet);
         //update score:
         //1. get the current score
-
-        const currentScoreElement = document.getElementById('current-score');
-        const currentScoreText = currentScoreElement.innerText;
-        const currentScore = parseInt(currentScoreText);
-        console.log(currentScore);
+        const currentScore = getTextElementvalueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score',updatedScore);
 
 
-        //2.increase the score by 1
-        const newScore = currentScore +1;
 
-        //3.show the update score
-        currentScoreElement.innerText = newScore;
+//----------------------------------------------------------------------------------
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+//-----------------------------------------------------------------------------------
+
+        // //2.increase the score by 1
+       
+
+        // //3.show the update score
+        // currentScoreElement.innerText = newScore;
 
 
 
@@ -50,18 +56,26 @@ function handleKeyboardKeyUp(event){
     }
     else{
         console.log('dhur vai/ apu right key press korun');
+        const currentLife = getTextElementvalueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+
+
+
+        //===================================================================
         //step 1: get the current life number
-        const currentLifeElement = document.getElementById('current-life');
-        const currentLifeText = currentLifeElement.innerText;
-        const currentLife = parseInt(currentLifeText);
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
 
 
-        //step 2: reduce the life count
-        const newLife = currentLife - 1;
+        // //step 2: reduce the life count
+        // const newLife = currentLife - 1;
 
 
-        //step 3: display the updated life count 
-        currentLifeElement.innerText = newLife;
+        // //step 3: display the updated life count 
+        // currentLifeElement.innerText = newLife;
     }
    
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -16,11 +16,17 @@ function removeBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-500');
 }
+function getTextElementvalueById (elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
 
-
-
-
-
+function setTextElementValueById (elementId,value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
 
 function getARandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
